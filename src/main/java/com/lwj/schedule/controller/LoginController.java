02423a7eeb694a37e1a944f96d751a5ac3ba9430
clouds <2270056326@qq.com>
2023-03-29 16:27:55 +0800

    yxf分支

--- src/main/java/com/lwj/schedule/controller/LoginController.java
+++ src/main/java/com/lwj/schedule/controller/LoginController.java
@@ -2,12 +2,14 @@ package com.lwj.schedule.controller;
 
 import com.lwj.schedule.dto.RespBean;
 import com.lwj.schedule.service.EmployeeService;
+import com.lwj.schedule.utils.CORSConfig;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @RestController
 @Slf4j
@@ -16,12 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private CORSConfig corsConfig;
+
 
     @ApiOperation(value="用户登录")
     @GetMapping("/Login")
     public RespBean Login(@RequestParam("Employee_id") String Employee_id, @RequestParam("Employee_pwd") String Employee_pwd){
+        corsConfig.addCorsMappings(new CorsRegistry());
         return employeeService.Login(Employee_id, Employee_pwd);
-
     }
 
 }
